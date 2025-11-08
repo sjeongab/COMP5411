bea686623ebd35565786f895fb381b38e64659b8
aliabdollahi024 <46813500+aliabdollahi024@users.noreply.github.com>
2025-11-08 16:53:33 +0800

    Update raytracingVertexShader.js

--- raytracing/raytracingVertexShader.js
+++ raytracing/raytracingVertexShader.js
@@ -1,8 +1,4 @@
-// raytracingVertexShader.js
-
-// برای glslVersion: THREE.GLSL3 باید ورودی position را تعریف کنیم
 const raytracingVertexShader = `
-
 void main() {
     gl_Position = vec4(position, 1.0);
 }
